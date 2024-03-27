0ed3ff8b64d8a5a22501c4fd8ce74cfc3c891f2c
holenya123a <nholenya@gmail.com>
2024-03-27 21:42:23 +0100

    added new fuction to post a new category

--- task_api/routes/category_routes.py
+++ task_api/routes/category_routes.py
@@ -1,16 +1,49 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from task_api.models.database import db_session
+from task_api.models.category import Category
 from sqlalchemy.exc import SQLAlchemyError
 
 # Create a Blueprint called 'tasks' to organize routes related to tasks
 category_bp = Blueprint('category', __name__)
 
 
-# Route to display all tasks
-@category_bp.route('/categorys', methods=['GET'])
-def get_all_categorys():
+# Route to display all categories
+@category_bp.route('/categories', methods=['GET'])
+def get_all_categories():
     try:
-        categorys = Category.query.get_all()
+        # Retrieve all categories from the database
+        categories = Category.query.all()
+
+        # Use a list comprehension to create the list of category data
+        categories_list: list = [{'category_id': category.category_id,
+                                  'name': category.name}
+                                 for category in categories]
+
+        # Return JSON response with list of categories
+        return jsonify({'categories': categories_list}), 200
+    except SQLAlchemyError as e:
+        # Handle database errors
+        return jsonify({'error': f'Error retrieving categories: {e}'}), 500
+
+
+# Route to add a new category
+@category_bp.route('/categories', methods=['POST'])
+def add_new_category():
+    try:
+        request_body = request.json()
+        name = request_body.get('name')
+
+        if not name or name is not str:
+            return jsonify({"message": "Missing valid 'name' field"}), 400
+
+        # Add new category to the database
+        new_category = Category(name=name)
+        db_session.add(new_category)
+        db_session.commit()
+
+        # Return successful creation message with category id
+        return jsonify({'message': 'Category added succesfully'}), 201
 
     except SQLAlchemyError as e:
-        return jsonify({'message': f'Errore retrieving user: {e}'}), 500
+        db_session.rollback()
+        return jsonify({'error': str(e)}), 500
